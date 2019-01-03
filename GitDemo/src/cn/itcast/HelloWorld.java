60a866340dafc1beb074c5f5b1b536f25576971f
wujiang <1245520307@qq.com>
2019-01-03 19:15:03 +0800

    添加你好世界

--- GitDemo/src/cn/itcast/HelloWorld.java
+++ GitDemo/src/cn/itcast/HelloWorld.java
@@ -2,6 +2,6 @@ package cn.itcast;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld11111111");
+        System.out.println("HelloWorld1111111111115454984654846516549846541+198494165");
     }
 }
