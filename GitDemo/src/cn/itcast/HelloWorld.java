8065f5a90948a632c327a7ad529d0e1e0c7daa56
wujiang <1245520307@qq.com>
2019-01-04 10:12:22 +0800

    ....

--- GitDemo/src/cn/itcast/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package cn.itcast;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld1111111111115454984654846516549846541+198494165");
-    }
-}
